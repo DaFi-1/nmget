add2a5d8ae3454763a08bb3706121f03dfd8d40b
DaFi <278515789+DaFi-1@users.noreply.github.com>
2026-08-08 19:40:21 -0300

    feat: CORS headers for capture script

--- main.py
+++ main.py
@@ -306,3 +306,11 @@ def nmget():
         return jsonify({"ok": True})
     return render_template('nmget.html')
 
+
+@app.after_request
+def add_cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+    return response
+
